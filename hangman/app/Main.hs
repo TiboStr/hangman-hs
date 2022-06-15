f9d42a0c9054a0eaaa0fae85e333b2dec7634dab
Tibo Stroo <tibo.stroo@hotmail.be>
2022-06-15 23:06:11 +0200

    Detect whether you win or lose and show correct screen

--- hangman/app/Main.hs
+++ hangman/app/Main.hs
@@ -10,18 +10,50 @@ import           Data.Text
 import           Data.Text.Internal
 import           System.Directory
 import qualified GI.Gtk             as Gtk
+import GHC.Base (undefined)
+
+data State = Playing Text [Char] | Won Text | Lost Text
 
 main :: IO ()
 main = do
-  loop "aac" []
+  loop (Playing "aac" [])
 
-loop :: Text -> [Char] -> IO ()
-loop word guesses = do
+loop :: State -> IO ()
+loop (Won word) = do
   Gtk.init Nothing
 
   win <- Gtk.windowNew Gtk.WindowTypeToplevel
 
-  Gtk.setContainerBorderWidth win 10
+  lbl <- Gtk.labelNew Nothing
+  Gtk.labelSetMarkup lbl $ append "You won!, You guessed the word \"" $ append word "\" correctly."
+
+  #add win lbl
+
+  Gtk.onWidgetDestroy win Gtk.mainQuit
+  #showAll win
+  Gtk.main
+
+loop (Lost word) = do
+  Gtk.init Nothing
+
+  win <- Gtk.windowNew Gtk.WindowTypeToplevel
+
+  lbl <- Gtk.labelNew Nothing
+  Gtk.labelSetMarkup lbl $ append "You lose., You couldn't guess the word \"" $ append word  "\"."
+
+  #add win lbl
+
+  Gtk.onWidgetDestroy win Gtk.mainQuit
+  #showAll win
+  Gtk.main
+
+
+loop (Playing word guesses) = do
+  Gtk.init Nothing
+
+  win <- Gtk.windowNew Gtk.WindowTypeToplevel
+
+  Gtk.setContainerBorderWidth win 10  -- TODO: in een functie steken
   Gtk.setWindowTitle win "Hangman"
   Gtk.setWindowResizable win False
   Gtk.setWindowDefaultWidth win 750
@@ -43,7 +75,7 @@ loop word guesses = do
   Gtk.gridSetRowSpacing grid 10
   Gtk.gridSetColumnHomogeneous grid True
 
-  fillGrid grid ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'] win word guesses
+  fillGrid grid ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'] win (Playing word guesses)
 
   #attach grid wordDisplay 0 0 13 1  -- col row width height
   #attach grid alreadyGuessed 0 1 13 1
@@ -56,25 +88,35 @@ loop word guesses = do
   Gtk.main
 
 
-fillGrid :: Gtk.Grid -> [Char] -> Gtk.Window -> Text -> [Char] -> IO ()
-fillGrid grid letters win word guesses = do
+fillGrid :: Gtk.Grid -> [Char] -> Gtk.Window -> State -> IO ()
+fillGrid grid letters win (Playing word guesses) = do
   let rows = Data.List.splitAt (Data.List.length letters `div` 2) letters
-  fillGridRow grid (fst rows) win word guesses 3 0
-  fillGridRow grid (snd rows) win word guesses 4 0
+  fillGridRow grid (fst rows) win (Playing word guesses) 3 0
+  fillGridRow grid (snd rows) win (Playing word guesses) 4 0
 
-  
-fillGridRow :: Gtk.Grid -> [Char] -> Gtk.Window -> Text -> [Char] -> Int -> Int -> IO ()
-fillGridRow grid letters win word guesses row index = do
+fillGrid _ _ _ _ = undefined
+
+
+fillGridRow :: Gtk.Grid -> [Char] -> Gtk.Window -> State -> Int -> Int -> IO ()
+fillGridRow grid letters win (Playing word guesses) row index = do
   btn <- Gtk.buttonNew
   Gtk.buttonSetLabel btn $ toUpper $ pack [letters !! index]
-  on btn #clicked $ do
-    Gtk.windowClose win
-    loop word $ letters !! index : guesses
+  on btn #clicked $ onButtonClick win (letters !! index) word guesses
   #attach grid btn (fromIntegral index) (fromIntegral row) 1 1
 
-  Control.Monad.when (index < Data.List.length letters - 1) $ fillGridRow grid letters win word guesses row $ index + 1
+  Control.Monad.when (index < Data.List.length letters - 1) $ fillGridRow grid letters win (Playing word guesses) row $ index + 1
 
+fillGridRow _ _ _ _ _ _ = undefined
 
+onButtonClick :: Gtk.Window -> Char -> Text -> [Char] -> IO ()
+onButtonClick win letter word guesses = do
+  Gtk.windowClose win
+  let newGuesses = if letter `Data.List.elem` guesses then  guesses else letter : guesses
+
+  let action | numberOfMisses word guesses >= 8 = loop (Lost word)
+             | hasWon word newGuesses = loop (Won word)
+             | otherwise = loop (Playing word newGuesses)
+  action
 
 hideGuessedCharsInWord :: Text -> [Char] -> Text
 hideGuessedCharsInWord word guessed =
@@ -90,3 +132,6 @@ numberOfMisses :: Text -> [Char] -> Int
 numberOfMisses word guessed =
     Data.Text.length $ Data.Text.filter (\c -> not $ c `Data.Text.elem` word) $ pack guessed
 
+
+hasWon :: Text -> [Char] -> Bool
+hasWon word guessed = Data.Text.null $ Data.Text.filter (`notElem` guessed) word
